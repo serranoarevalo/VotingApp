fc513c93596878275d403c674147ff2ff6c34fbd
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-06 13:04:52 +0700

    Getting winner or ties, test and function

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -19,3 +19,23 @@ export function vote(state, entry) {
 		tally => tally + 1
 	);
 }
+
+function getWinners(vote) {
+	if(!vote) return [];
+	const [a, b] = vote.get('pair');
+	const aVotes = vote.getIn(['tally', a], 0);
+	const bVotes = vote.getIn(['tally', b], 0);
+
+	if 			(aVotes > bVotes) 	return [a];
+	else if 	(aVotes < bVotes) 	return [b];
+	else 							return [a,b];
+}
+
+export funciont next(state) {
+	const entries = state.get('entries')
+					.contact(getWinners(state.get('vote')));
+	return state.merge({
+		vote: Map({pair: entries.take(2)}),
+		entries: entries.skip(2)
+	})
+}
