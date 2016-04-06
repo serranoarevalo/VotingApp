689e78bf70de93a9171f5b205e2620570b71ed18
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-06 12:13:32 +0700

    SetEntries and Next pure immutable functions written, with tests

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
 
@@ -25,9 +25,28 @@ describe('application logic', () => {
 			const nextState = setEntries(state, entries);
 			expect(nextState).to.equal(Map({
 				entries: List.of('Trainspotting', '28 Days Later')
-			}))
+			}));
+		});
+	});
+
+	describe('next', () => {
+
+		it('takes the next two entries under vote', () => {
+
+			const state = Map({
+				entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+			});
+
+			const nextState = next(state);
+
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later')
+				}),
+				entries: List.of('Sunshine')
+			}));
+
 		})
 
-	});
-;
-})
+	})
+});
