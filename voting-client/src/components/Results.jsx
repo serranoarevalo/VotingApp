b9cd2e1f51214c8022a5c2fb25e2ae190902b926
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-07 11:01:47 +0700

    Results test with clicking button

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import reactMixin from 'react-mixin';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Winner from './Winner';
 
 export default class Results extends React.Component {
 	getPair() {
@@ -14,7 +15,9 @@ export default class Results extends React.Component {
 	}
 	render() {
 		return (
+			this.props.winner ? <Winner ref="winner" winner={this.props.winner} /> :
 			<div className="results">
+				<div className="tally">
 				{this.getPair().map(entry => 
 					<div key={entry} className="entry">
 						<h1>{entry}</h1>
@@ -23,7 +26,16 @@ export default class Results extends React.Component {
 						</div>
 					</div>
 				)}
+				</div>
+				<div className="management">
+					<button ref="next"
+							className="next"
+							onClick={this.props.next}>
+					Next
+					</button>
+				</div>
 			</div>
+			
 		)
 	}
 }
